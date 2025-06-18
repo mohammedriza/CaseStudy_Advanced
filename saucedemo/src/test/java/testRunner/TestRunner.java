6aa6bd083e12e4f8aa39e708f53d6f0deb2b622e
Administrator <riza.mohammed@hotmail.com>
2025-06-18 22:47:35 +0530

    Added TestNg file and run via Test Suite

--- saucedemo/src/test/java/testRunner/TestRunner.java
+++ saucedemo/src/test/java/testRunner/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/features"},
-        glue = {"org.example.cucumber.steps"},
+        glue = {"cucumber.steps"},
         plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"},
         tags = "@Functional",
         monochrome = true)
